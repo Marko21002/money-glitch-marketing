00163db0f52b0f4a63e28e7e94f7c3c8b9b5bf2e
Marko21002 <markgufman21@gmail.com>
2024-04-13 03:44:37 +0200

    feateru text fix2

--- components/ui/feature-creator.tsx
+++ components/ui/feature-creator.tsx
@@ -45,18 +45,18 @@ export default function FeatureCreator() {
             <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 dark:text-white">Features for Creators</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight dark:text-white sm:text-4xl">Empower your creativity</p>
-              <p className="mt-6 text-lg leading-8 text-white">
+              <p className="mt-6 text-lg leading-8 dark:text-white">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                 iste dolor cupiditate blanditiis ratione.
               </p>
-              <dl className="mt-10 space-y-8 text-base leading-7 text-white lg:max-w-none">
+              <dl className="mt-10 space-y-8 text-base leading-7 dark:text-whitelg:max-w-none">
                 {creatorFeatures.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold dark:text-white">
                       <div className='absolute left-1 top-1 h-5 w-5'>{feature.icon}</div>
                       {feature.name}
                     </dt>
-                    <dd className="inline text-white">{feature.description}</dd>
+                    <dd className="inline dark:text-white">{feature.description}</dd>
                   </div>
                 ))}
               </dl>
